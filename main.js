1f8bf902a40241e3dacdfab417a41f70579f7a65
omic <esther.zh.wenting@gmail.com>
2013-10-25 15:59:56 -0400

    table shown on webpage, include "ns-table.js" as module

---
+++ main.js
@@ -0,0 +1,9 @@
+    // main.js
+    var app = angular.module('myApp', ['ngTable']);
+    app.controller('MyCtrl', function($scope) {
+    $scope.users = [{name: "Moroni", age: 50},
+    {name: "Tiancum", age: 43},
+    {name: "Jacob", age: 27},
+    {name: "Nephi", age: 29},
+    {name: "Enos", age: 34}];
+    });
